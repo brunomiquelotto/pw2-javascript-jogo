0f72d1837817c2dcdf68ff503a7a591347d7e550
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2017-05-10 21:07:55 -0300

    Ranking Funcional

---
+++ app/js/ranking.js
@@ -0,0 +1,33 @@
+//Jogador = {nome: "Nome", ra:"94426", pontos}; Estrutura do objeto jogador
+var ranking = [];
+var maximoJogadoresRanking = 5;
+
+function addNovoJogador(jogador) {
+	ranking.push(jogador);
+	ordenaRanking();
+	if (ranking.length > maximoJogadoresRanking) ranking.pop();
+	modificaHTML('listaRank');
+}
+
+function ordenaRanking() {
+	ranking.sort(function(a, b) {
+		return b.pontos - a.pontos;
+	});
+}
+
+function modificaHTML(id) {
+	var lista = document.getElementById(id);
+	lista.innerHTML = "";
+	ranking.forEach(function(jogador,index) {
+		var element = document.createElement("li");
+		var span1 = document.createElement("span");
+		span1.innerText = jogador.nome;
+		span1.className += 'rnome';
+		element.appendChild(span1);
+		var span2 = document.createElement("span");
+		span2.innerHTML = jogador.pontos;
+		span2.className += 'rpontos';
+		element.appendChild(span2);
+		lista.appendChild(element);
+	});
+}
